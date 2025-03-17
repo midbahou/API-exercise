25bc847b68bdb59ff509341f0e94b27de7344f29
midbahou <midbahou@gmail.com>
2025-03-17 16:23:40 -0400

    updated

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import router from "./routes/grades.js";
 dotenv.config();
+
+console.log(process.env.ATLAS_URI);
+
+
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -10,6 +15,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Welcome to the API.");
 });
+
+app.use('/grades', router);
 
 // Global error handling
 app.use((err, _req, res, next) => {
