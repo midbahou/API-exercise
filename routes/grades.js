a4c94027d9ef0ae06c426a592cef5ebf85daaf8d
midbahou <midbahou@gmail.com>
2025-03-18 11:15:10 -0400

    grades

--- routes/grades.js
+++ routes/grades.js
@@ -4,6 +4,7 @@ import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
+// GET /grades/:id
 // Get a single grade entry
 router.get("/:id", async (req, res) => {
   let collection = await db.collection("grades");
@@ -12,6 +13,46 @@ router.get("/:id", async (req, res) => {
 
   if (!result) res.send("Not found").status(404);
   else res.send(result).status(200);
+});
+
+// student route for backwards compatibility
+router.get('/student/:id', async(req, res) => {
+  res.redirect(`/grades/learner/${req.params.id}`);
+})
+
+// Get a student's grade data
+router.get("/learner/:id", async (req, res) => {
+  let collection = await db.collection("grades");
+  let query = { learner_id: Number(req.params.id) };
+  let result = await collection.find(query).toArray();
+
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
+// Get a class's grade data
+router.get("/class/:id", async (req, res) => {
+  let collection = await db.collection("grades");
+  let query = { class_id: Number(req.params.id) };
+  let result = await collection.find(query).toArray();
+
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
+// Create a single grade entry
+router.post("/", async (req, res) => {
+  let collection = await db.collection("grades");
+  let newDocument = req.body;
+
+  // rename fields for backwards compatibility
+  if (newDocument.student_id) {
+    newDocument.learner_id = newDocument.student_id;
+    delete newDocument.student_id;
+  }
+
+  let result = await collection.insertOne(newDocument);
+  res.send(result).status(204);
 });
 
 export default router;
